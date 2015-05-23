8b4fa3f509172e1128baaadee2f3380bfa753ab1
iev <iev@land.ru>
2015-05-23 19:55:30 +0300

    asyncio for control dlgs

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 
 from . import gui, srv
 from util.columns import *
-from util.misc import app_devtypes
+from util.misc import app_devtypes, app_devdata
 from .regs import startup_cb
 
-devdata = lambda: get_devdata('SG', get_columns([c_ip_addr, c_spi, c_gpio, c_refin]), app_devtypes(gui))
+devdata = lambda: app_devdata('SG', get_columns([c_ip_addr, c_spi, c_gpio, c_refin]), app_devtypes(gui))
 
