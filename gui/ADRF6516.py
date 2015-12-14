bdd93278bc382ef5411bbb5ff69b125a410fca81
iev <iev@land.ru>
2015-12-14 19:49:36 +0300

    added sg/ADRF6516

---
+++ gui/ADRF6516.py
@@ -0,0 +1,58 @@
+
+from collections import OrderedDict as OD
+
+from util import Data, control_cb, util_io_cb
+from util.columns import *
+from .callbacks import spi_efc_cmd_cb
+
+gain_list = OD()
+gain_list[2] = ['3dB', '6dB']
+gain_list[1] = ['6dB', '12dB']
+gain_list[0] = ['22dB', '28dB']
+
+def gain_fmt_cb(val, read=True, n=0):
+    if read:
+        val = int(val, 16)
+        return gain_list[n][val >> n & 1]
+    else:
+        gl = gain_list[n]
+        gain_fmt_cb.gain[n] = 0 if val == gl[0] else 1
+        return '%d' % gain_fmt_cb.gain[n]
+gain_fmt_cb.gain = OD()
+
+def lpf_fmt_cb(val, read=True):
+    if read:
+        val = int(val, 16)
+        val >>= 3
+        val &= 0x1F
+        return '%d' % val
+    else:
+        val = int(val)
+        val &= 0x1F
+        val <<= 3
+        val |= 0x80
+        val |= gain_fmt_cb.gain[2] << 2
+        val |= gain_fmt_cb.gain[1] << 1
+        val |= gain_fmt_cb.gain[0] << 0
+        return '%.2X' % val
+
+def lpf_cmd_cb(dev, cmd, val=None):
+    if val == None:
+        val = 'FF'
+    return spi_efc_cmd_cb(dev, cmd, val)
+
+def columns():
+    return get_columns([c_ip_addr, c_spi])
+
+def get_menu(dev):
+    return OD([('Control', control_cb)])
+
+def get_ctrl(dev):
+    data = Data(name='LPF', io_cb=util_io_cb)
+    data.add('lpf', label='LPF, MHz', wdgt='spin', value={'min':0, 'max':31, 'step':1}, fmt_cb=lpf_fmt_cb, cmd_cb=lpf_cmd_cb, send=True)
+    data.add('g3', label='Preamplifier gain', wdgt='radio', value=gain_list[2], fmt_cb=lambda v,r: gain_fmt_cb(v,r,2), send=False)
+    data.add('g2', label='Postamplifier gain', wdgt='radio', value=gain_list[1], fmt_cb=lambda v,r: gain_fmt_cb(v,r,1), send=False)
+    data.add('g1', label='VGA max gain', wdgt='radio', value=gain_list[0], fmt_cb=lambda v,r: gain_fmt_cb(v,r,0), send=False)
+
+    return data
+
--- gui/ADRF6820/ADRF6820.py
+++ gui/ADRF6820/ADRF6820.py
@@ -6,6 +6,43 @@ from ..callbacks import spi_efc_cmd_cb
 from . import ADRF6820_hex, ADRF6820_bin
 from util.columns import *
 
+refsel_list = ['x2', 'x1', '/2', '/4', '/8']
+
+def Fpfd_src_cb(data, val):
+    REFSEL = data.get_value('REFSEL')
+    Fpfd = float(data.get_value('REFin'))
+    if REFSEL[0] == 'x':
+        Fpfd *= int(REFSEL[1])
+    else:
+        Fpfd /= int(REFSEL[1])
+    return '%.2f' % Fpfd
+
+def Fvco_src_cb(data, val):
+    Fpfd = float(data.get_value('Fpfd'))
+    INT = float(data.get_value('INT'))
+    FRAC = float(data.get_value('FRAC'))
+    MOD = float(data.get_value('MOD'))
+    Fvco = Fpfd*2*(INT + FRAC/MOD)
+    return '%.3f' % Fvco
+
+def get_calc_data():
+    data = RegsData(sz=16)
+    data.add_page('calc0')
+    data.add('label1', label='Fvco = 2 x Fpfd x (INT + FRAC/MOD )')
+    data.add_page('calc1')
+    data.add('Fvco', wdgt='entry', state='readonly', msg='Fvco', src=Fvco_src_cb)
+    data.add('Fpfd1', wdgt='entry', state='readonly', msg='Fpfd', src=Fpfd_src_cb)
+    data.add('INT', wdgt='spin', value={'min':21, 'max':123, 'step':1}, msg='INT', src=lambda d,v: d.bits_src('R02', 0, 10, v))
+    data.add('FRAC', wdgt='spin', value={'min':0, 'max':65535, 'step':1}, msg='FRAC', src=lambda d,v: d.bits_src('R03', 0, 15, v))
+    data.add('MOD', wdgt='spin', value={'min':1, 'max':65535, 'step':1}, msg='MOD', src=lambda d,v: d.bits_src('R04', 0, 15, v))
+    data.add_page('calc2')
+    data.add('label0', label='Fpfd = REFin x REFSEL')
+    data.add_page('calc3')
+    data.add('Fpfd', wdgt='entry', state='readonly', msg='Fpfd', src=Fpfd_src_cb)
+    data.add('REFin', wdgt='entry', state='readonly', msg='REFin', src=lambda d,v: d.dev_src('refin'))
+    data.add('REFSEL', wdgt='combo', state='readonly', value=refsel_list, msg='REFSEL', src=lambda d,v: d.list_src('R21', 0, 2, refsel_list, v))
+    return data
+
 def columns():
     return get_columns([c_ip_addr, c_spi, c_refin])
 
@@ -13,7 +50,7 @@ def get_menu(dev):
     return OD([('Registers', manyregs_cb)])
 
 def get_regs(dev):
-    data = RegsData(sz=16)
+    data = get_calc_data()
     data.columns = 5
     data.add_hex_data(ADRF6820_hex.data, cmd_cb=spi_efc_cmd_cb)
     data.add_bin_data(ADRF6820_bin.data)
